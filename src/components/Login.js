eb79fe909e4ec6b596f44d49e048990ea56ecbd4
nganph5 <nganp@email.arizona.edu>
2022-05-07 22:38:28 -0400

    Restyle login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,56 +1,111 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Container } from "react-bootstrap";
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import APIAccess from '../communication/APIAccess';
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBCheckbox,
+  MDBBtn,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
+import congifuration from "../configuration";
+import APIAccess from "../communication/APIAccess";
 
 function Login(props) {
-  const [password, setPass] = useState('');
-  const [email, setEmail] = useState('');
+  const [password, setPass] = useState("");
+  const [email, setEmail] = useState("");
   const navigate = useNavigate();
 
   let onEmailChanged = (e) => {
     setEmail(e.target.value);
-  }
+  };
 
   let onPassChanged = (e) => {
     setPass(e.target.value);
-  }
+  };
 
   let onSubmitHandler = (e) => {
     e.preventDefault();
     APIAccess.login(email, password)
-    .then(x => {
-        if(x.done) {
-            props.customerLoggedIn(email);
-            navigate('/');           
+      .then((x) => {
+        if (x.done) {
+          props.customerLoggedIn(email);
+          navigate("/");
         } else {
-            alert('The credentials are not valid!');
+          alert("The credentials are not valid!");
         }
-    })
-    .catch(e => {
-        alert('Something went wrong!');
-    });         
-  }
+      })
+      .catch((e) => {
+        alert("Something went wrong!");
+      });
+  };
+  let google = `${congifuration.backendAddress}/auth/google`;
 
-  
+  let logout = () => {
+    APIAccess.logout()
+      .then((x) => props.customerLoggedOut())
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-  return(
-    <Container>
-      <Form onSubmit={onSubmitHandler}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter Email" value={email} onChange={onEmailChanged}/>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Enter Password" value={password} onChange={onPassChanged}/>
-          </Form.Group>   
-          <Button type="submit">Log In</Button>  
-      </Form> 
-    </Container>
+  return (
+    <>
+      <Container>
+        <form onSubmit={onSubmitHandler}>
+          <MDBInput
+            className="mb-4"
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={onEmailChanged}
+            id="form2Example1"
+            label="Email address"
+          />
+          <MDBInput
+            className="mb-4"
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={onPassChanged}
+            id="form2Example2"
+            label="Password"
+          />
+
+          <MDBRow className="mb-4">
+            <MDBCol className="d-flex justify-content-center">
+              <MDBCheckbox
+                id="form2Example3"
+                label="Remember me"
+                defaultChecked
+              />
+            </MDBCol>
+            <MDBCol>
+              <a href="#!">Forgot password?</a>
+            </MDBCol>
+          </MDBRow>
+
+          <MDBBtn type="submit" className="mb-4" block>
+            Sign in
+          </MDBBtn>
+
+          <div className="text-center">
+            <p>
+              Not a member? <Link to="/register">Register</Link>
+            </p>
+            <p>or sign up with:</p>
+
+            <MDBBtn floating className="mx-1">
+              <MDBIcon fab icon="google" />
+            </MDBBtn>
+          </div>
+        </form>
+      </Container>
+    </>
   );
 }
 
